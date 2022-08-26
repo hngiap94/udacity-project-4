82328d77d971d717a93f85795f7cb80ced56876f
hngiap94 <hngiap94@gmail.com>
2022-08-26 22:00:01 +0700

    complete run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=hngiap94/udacity-pj4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-pj4 --image=$dockerpath --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-pj4 8000:80
